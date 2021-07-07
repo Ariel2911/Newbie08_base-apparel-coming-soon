7fa3f9c01b43ab3bfe05116f53e9014766f3adb9
ariel <ariel29111978@hotmail.com>
2021-07-07 00:14:59 -0300

    Se agregó el archivo javascript y una clase CSS para interactuar con el DOM

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+form.addEventListener('submit',e =>{    
+    if(e.target.email.value.trim()==='' || emailRegex.test(e.target.value)){
+        e.preventDefault();
+        form.classList.add('main__body-form--error');
+    }
+});
+
+form.email.addEventListener('focus', () =>{
+    form.classList.remove('main__body-form--error');
+});
